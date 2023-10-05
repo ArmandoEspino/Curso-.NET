f220255dbeef499cb0e44719c60eaa3464e380eb
Armando <armandoerj7@gmail.com>
2023-10-04 21:45:00 -0600

    Actividad 2
    Proyecto BankSolution

--- BankSolution/BankConsole/Program.cs
+++ BankSolution/BankConsole/Program.cs
@@ -1,9 +1,109 @@
 ﻿using BankConsole;
 
-User james = new User(1,"James","james@gmail.com",1000);
-Console.WriteLine(james.ShowData());
+if(args.Length == 0)
+    EmailService.SendMail();
+else
+    ShowMenu();
 
-// Usamos el contructor:
+void ShowMenu(){
+    Console.Clear();
+    Console.WriteLine("Selecciona una opcion: ");
+    Console.WriteLine("1 - Crear nuevo usuario.");
+    Console.WriteLine("2 - Eliminar un usuario existente.");
+    Console.WriteLine("3 - Salir.");
 
-User alex = new User(2,"Alex","alex@gmail.com",3000);
-Console.WriteLine(alex.ShowData());
+    int option = 0;
+
+    do{
+        string input = Console.ReadLine();
+
+        if(!int.TryParse(input, out option))
+            Console.WriteLine("Debes ingresar un numero (1, 2 o 3).");
+        else if (option > 3)
+            Console.WriteLine("Debes ingresar un numero valido (1, 2 o 3).");
+
+    }while (option == 0 || option > 3);
+
+    switch (option)
+    {
+        case 1:
+            CreateUser();
+            break;
+        case 2:
+            DeleteUser();
+            break;
+        case 3:
+            Environment.Exit(0);
+            break;
+    }
+
+    void CreateUser(){
+
+        Console.Clear();
+        Console.WriteLine("Ingresa la informacion del usuario");
+
+        Console.Write("ID: ");
+        int ID = int.Parse(Console.ReadLine());
+
+        Console.Write("Nombre: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Email: ");
+        string email = Console.ReadLine();
+
+        Console.Write("Saldo: ");
+        decimal balance = decimal.Parse(Console.ReadLine());
+
+        Console.Write("Escribe 'c' si el usuario es un Cliente, 'e' si es un Empleado: ");
+        char userType = char.Parse(Console.ReadLine());
+
+        User newUser;
+
+        if(userType.Equals('c')){
+
+            Console.Write("Regime Fiscal: ");
+            char taxRegime = char.Parse(Console.ReadLine());
+
+            newUser = new Client(ID, name, email, balance, taxRegime);
+
+        }else{
+
+            Console.Write("Department: ");
+            string department = Console.ReadLine();
+
+            newUser = new Employee(ID, name, email, balance, department);
+        }
+
+        Storage.AddUser(newUser);
+
+        Console.WriteLine("Usuario creado.");
+        Thread.Sleep(2000);
+        ShowMenu();
+    }
+
+    void DeleteUser(){
+
+        Console.Clear();
+
+        Console.Write("Ingresa el ID del usuario a eliminar: ");
+        int ID = int.Parse(Console.ReadLine());
+
+        string result = Storage.DeleteUser(ID);
+
+        if (result.Equals("Success"))
+        {
+            Console.Write("Usuario eliminado");
+            Thread.Sleep(2000);
+            ShowMenu();
+        }
+    }
+}
+
+
+/*
+Client james = new Client(1,"James","james@gmail.com",1000, 'M');
+Employee alex = new Employee(2,"Alex","alex@gmail.com",1000, "IT");
+
+Storage.AddUser(james);
+Storage.AddUser(alex);
+*/
--- BankSolution/BankConsole/User.cs
+++ BankSolution/BankConsole/User.cs
@@ -1,42 +1,57 @@
+using Newtonsoft.Json;
+
 namespace BankConsole;
 
 public class User{
-    
-    private int ID { get; set;}
-
-    private string Name { get; set;}
-
-    private string Email { get; set;}
-
-    private decimal Balance { get; set;}
-
-    private DateTime RegisterDate { get; set;}
+    [JsonProperty]
+    protected int ID { get; set;}
+    [JsonProperty]
+    protected string Name { get; set;}
+    [JsonProperty]
+    protected string Email { get; set;}
+    [JsonProperty]
+    protected decimal Balance { get; set;}
+    [JsonProperty]
+    protected DateTime RegisterDate { get; set;}
 
 
     // Constructor
-
-    public User(){
-        this.Balance = 2000;
-    }
+    public User(){}
 
     public User(int ID, string Name, string Email, decimal Balance){
         this.ID = ID;
         this.Name = Name;
         this.Email = Email;
-        this.Balance = Balance;
         this.RegisterDate = DateTime.Now;
     }
 
+    public int GetID(){
+        return ID;
+    }
 
-    public void setBalance(decimal Balance){
-        if(Balance < 0){
-            this.Balance = 0;
+    public DateTime GetRegisterDate(){
+        return RegisterDate;
+    }
+
+
+    public virtual void SetBalance(decimal amount){
+
+        decimal quantity = 0;
+
+        if(amount < 0){
+            quantity = 0;
         }else{
-            this.Balance = Balance;
+            quantity = amount;
         }
+        this.Balance += quantity;
     }
 
-    public string ShowData(){
-        return $"\nNombre: {this.Name}\nCorreo: {this.Email}\nSaldo: {this.Balance}\nFecha de registro: {this.RegisterDate}\n";
+    public virtual string ShowData(){
+        return $"\nID: {this.ID}\nNombre: {this.Name}\nCorreo: {this.Email}\nSaldo: {this.Balance}\nFecha de registro: {this.RegisterDate.ToShortDateString()}\n";
     }
+
+    public virtual string ShowData(string InitialMessage){
+        return $"\n{InitialMessage} -> Nombre: {this.Name}\nCorreo: {this.Email}\nSaldo: {this.Balance}\nFecha de registro: {this.RegisterDate}\n";
+    }
+
 }
